33b9f9e173816df5ae42d1adf45653ddcfc9d943
Harsh.Trivedi <trivediharsh49@gmail.com>
2023-01-19 22:57:28 +0530

    Initialize release-0.3

---
+++ test/functional/terminal/window_spec.lua
@@ -0,0 +1,143 @@
+local helpers = require('test.functional.helpers')(after_each)
+local thelpers = require('test.functional.terminal.helpers')
+local feed, clear = helpers.feed, helpers.clear
+local wait = helpers.wait
+local iswin = helpers.iswin
+local command = helpers.command
+local retry = helpers.retry
+local eq = helpers.eq
+local eval = helpers.eval
+
+describe(':terminal window', function()
+  local screen
+
+  before_each(function()
+    clear()
+    screen = thelpers.screen_setup()
+  end)
+
+  it('sets topline correctly #8556', function()
+    -- Test has hardcoded assumptions of dimensions.
+    eq(7, eval('&lines'))
+    command('set shell=sh')
+    command('terminal')
+    retry(nil, nil, function() assert(nil ~= eval('b:terminal_job_pid')) end)
+    -- Terminal/shell contents must exceed the height of this window.
+    command('topleft 1split')
+    feed([[i<cr>]])
+    -- Check topline _while_ in terminal-mode.
+    retry(nil, nil, function() eq(6, eval('winsaveview()["topline"]')) end)
+  end)
+
+  describe("with 'number'", function()
+    it('wraps text', function()
+      feed([[<C-\><C-N>]])
+      feed([[:set numberwidth=1 number<CR>i]])
+      screen:expect([[
+        {7:1 }tty ready                                       |
+        {7:2 }rows: 6, cols: 48                               |
+        {7:3 }{1: }                                               |
+        {7:4 }                                                |
+        {7:5 }                                                |
+        {7:6 }                                                |
+        {3:-- TERMINAL --}                                    |
+      ]])
+      thelpers.feed_data({'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'})
+      screen:expect([[
+        {7:1 }tty ready                                       |
+        {7:2 }rows: 6, cols: 48                               |
+        {7:3 }abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUV|
+        {7:4 }WXYZ{1: }                                           |
+        {7:5 }                                                |
+        {7:6 }                                                |
+        {3:-- TERMINAL --}                                    |
+      ]])
+
+      if iswin() then
+        return  -- win: :terminal resize is unreliable #7007
+      end
+
+      -- numberwidth=9
+      feed([[<C-\><C-N>]])
+      feed([[:set numberwidth=9 number<CR>i]])
+      screen:expect([[
+        {7:       1 }tty ready                                |
+        {7:       2 }rows: 6, cols: 48                        |
+        {7:       3 }abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNO|
+        {7:       4 }WXYZrows: 6, cols: 41                    |
+        {7:       5 }{1: }                                        |
+        {7:       6 }                                         |
+        {3:-- TERMINAL --}                                    |
+      ]])
+      thelpers.feed_data({' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'})
+      screen:expect([[
+        {7:       1 }tty ready                                |
+        {7:       2 }rows: 6, cols: 48                        |
+        {7:       3 }abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNO|
+        {7:       4 }WXYZrows: 6, cols: 41                    |
+        {7:       5 } abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMN|
+        {7:       6 }OPQRSTUVWXYZ{1: }                            |
+        {3:-- TERMINAL --}                                    |
+      ]])
+    end)
+  end)
+
+  describe("with 'colorcolumn'", function()
+    before_each(function()
+      feed([[<C-\><C-N>]])
+      screen:expect([[
+        tty ready                                         |
+        {2:^ }                                                 |
+                                                          |
+                                                          |
+                                                          |
+                                                          |
+                                                          |
+      ]])
+      feed(':set colorcolumn=20<CR>i')
+    end)
+
+    it('wont show the color column', function()
+      screen:expect([[
+        tty ready                                         |
+        {1: }                                                 |
+                                                          |
+                                                          |
+                                                          |
+                                                          |
+        {3:-- TERMINAL --}                                    |
+      ]])
+    end)
+  end)
+
+  describe('with fold set', function()
+    before_each(function()
+      feed([[<C-\><C-N>:set foldenable foldmethod=manual<CR>i]])
+      thelpers.feed_data({'line1', 'line2', 'line3', 'line4', ''})
+      screen:expect([[
+        tty ready                                         |
+        line1                                             |
+        line2                                             |
+        line3                                             |
+        line4                                             |
+        {1: }                                                 |
+        {3:-- TERMINAL --}                                    |
+      ]])
+    end)
+
+    it('wont show any folds', function()
+      feed([[<C-\><C-N>ggvGzf]])
+      wait()
+      screen:expect([[
+        ^tty ready                                         |
+        line1                                             |
+        line2                                             |
+        line3                                             |
+        line4                                             |
+        {2: }                                                 |
+                                                          |
+      ]])
+    end)
+  end)
+end)
+
